a1fb2d59b52a652e8c0f6f65f87768a3d88b6453
MatteoSanson <matteo.sanson@gmail.com>
2024-01-15 17:39:48 +0100

    feat: struttura models

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php
 
-class Movie {
-    // variabili d'istanza
-    public $title;
-    public $director;
-    public $releaseYear;
-    public $genre;
-
-    // costruttore
-    function __construct($_title, $_director, $_releaseYear, $_genre){
-        $this -> title = $_title;
-        $this -> director = $_director;
-        $this -> releaseYear = $_releaseYear;
-        $this -> genre = $_genre;
-    }
-
-    // metodo
-    public function showInfo() {
-        echo "Titolo: {$this -> title} <br>";
-        echo "Regista: {$this -> director} <br>";
-        echo "Anno: {$this -> releaseYear} <br>";
-        echo "Genere: {$this -> genre} <br> <hr>";
-    }
-}
-
+require_once __DIR__ . '/models/movie.php';
 
 $interstellar = new Movie("Interstellar", "Christopher Nolan", 2014, "Fantascienza");
 $la_migliore_offerta = new Movie("La Migliore Offerta", "Giuseppe Tornatore", 2013, "Thriller");
@@ -33,5 +10,3 @@ $interstellar -> showInfo();
 
 echo "Film 2: <br><br>";
 $la_migliore_offerta -> showInfo();
-
-
---
+++ models/movie.php
@@ -0,0 +1,25 @@
+<?php
+
+class Movie {
+    // variabili d'istanza
+    public $title;
+    public $director;
+    public $releaseYear;
+    public $genre;
+
+    // costruttore
+    function __construct($_title, $_director, $_releaseYear, $_genre){
+        $this -> title = $_title;
+        $this -> director = $_director;
+        $this -> releaseYear = $_releaseYear;
+        $this -> genre = $_genre;
+    }
+
+    // metodo
+    public function showInfo() {
+        echo "Titolo: {$this -> title} <br>";
+        echo "Regista: {$this -> director} <br>";
+        echo "Anno: {$this -> releaseYear} <br>";
+        echo "Genere: {$this -> genre} <br> <hr>";
+    }
+}
