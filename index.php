3fc02109c9799424129c1d36c5b6b7789e32098a
MatteoSanson <matteo.sanson@gmail.com>
2024-01-15 15:38:35 +0100

    feat: costruttore classe Movie

--- index.php
+++ index.php
@@ -3,15 +3,31 @@
 class Movie {
     // variabili d'istanza
     public $title;
-    public $genre;
-    public $releaseYear;
     public $director;
+    public $releaseYear;
+    public $genre;
+
+    function __construct($_title, $_director, $_releaseYear, $_genre){
+        $this -> title = $_title;
+        $this -> director = $_director;
+        $this -> releaseYear = $_releaseYear;
+        $this -> genre = $_genre;
+    }
 }
 
-$interstellar = new Movie();
-$interstellar->title = "Interstellar";
-$interstellar->genre = "Fantascienza";
-$interstellar->releaseYear = "2014";
-$interstellar->director = "Christopher Nolan";
+// $interstellar = new Movie();
+// $interstellar->title = "Interstellar";
+// $interstellar->director = "Christopher Nolan";
+// $interstellar->releaseYear = "2014";
+// $interstellar->genre = "Fantascienza";
 
-var_dump($interstellar);
+
+// var_dump($interstellar);
+
+$interstellar = new Movie("Interstellar", "Christopher Nolan", "2014", "Fantascienza");
+echo "Title: " . $interstellar -> title . "<br>";
+echo "Director: " . $interstellar -> director . "<br>";
+echo "Release Year: " . $interstellar -> releaseYear . "<br>";
+echo "Genre: " . $interstellar -> genre . "<br>";
+
+
