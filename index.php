6506dae68f271350fc3a534c17586e5ae53eb5b1
MatteoSanson <matteo.sanson@gmail.com>
2024-01-15 16:18:55 +0100

    feat: milestone1

--- index.php
+++ index.php
@@ -26,8 +26,12 @@ class Movie {
 
 
 $interstellar = new Movie("Interstellar", "Christopher Nolan", 2014, "Fantascienza");
+$la_migliore_offerta = new Movie("La Migliore Offerta", "Giuseppe Tornatore", 2013, "Thriller");
 
 echo "Film 1: <br><br>";
 $interstellar -> showInfo();
+
+echo "Film 2: <br><br>";
+$la_migliore_offerta -> showInfo();
 
 
